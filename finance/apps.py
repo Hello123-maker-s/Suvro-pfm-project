ff8db49f0e75fe834f0a995f49e7b48013c1ebe4
Hello-maker-s <subtilt75@gmail.com>
2025-12-18 03:43:21 +0530

    just fix

--- finance/apps.py
+++ finance/apps.py
@@ -5,6 +5,4 @@ class FinanceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'finance'
     
-    def ready(self):
-        from ml.classifier import load_classifier
-        load_classifier()
+
